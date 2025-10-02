c61612bb4d68b563b837418780f1701f986c272f
unknown <kavishkajayawardana34@gmail.com>
2025-10-02 06:29:15 +0530

    Kavishka Commit 4

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -6,69 +6,119 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStack } from "../../App";
 import { useNavigation } from "@react-navigation/native";
 import CountryPicker, { Country, CountryCode } from "react-native-country-picker-modal";
+
 import { useUserRegistration } from "../components/UserContext";
+import { ALERT_TYPE, AlertNotificationRoot, Toast } from "react-native-alert-notification";
+import { validateCountryCode, validatePhoneNo } from "../../util/Validation";
 
 type ContactProps = NativeStackNavigationProp<RootStack, "AvatarScreen">;
 
-
 export default function ContactScreen() {
-
     const navigation = useNavigation<ContactProps>();
-    const [countryCode, setCountryCode] = useState<CountryCode>("LK"); 
-    
+    const [countryCode, setCountryCode] = useState<CountryCode>("LK");
+
     const [country, setCountry] = useState<Country | null>(null);
     const [show, setShow] = useState<boolean>(false);
+    const { userData, setUserData } = useUserRegistration();
 
+    const [callingCode, setCallingCode] = useState("+94");
+    const [phoneNo, setPhoneNo] = useState("");
 
-    const {userData, setUserData} = useUserRegistration();
     return (
-    
-    <SafeAreaView>
-        <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "android" ? 100 : 100 }>
-            
-            <View className="p-5 items-center">
-                <View>
-                    
-                    <Image source={require("../../assets/logo.png")} className="h-40 w-36"/>
-                </View>
-                <View>
-                    
-                    <Text className="text-slate-600 font-bold">
-                        
-                        We use your contacts to help aaaaaaaayou find friends who are already on the app. Your contacts stay private.
-                    </Text>
-                </View>
 
-                
-                <View className="mt-5 w-full">
+        <SafeAreaView className="flex-1 bg-white items-center">
+            <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "android" ? 100 : 100}>
 
-                    <Pressable className="bg-red-100 w-full justify-center items-center h-16">
-                        
-                        <Text className="font-bold text-lg">Select Country</Text>
-                        <AntDesign name="caret-down" size={24} color="black" style={{ marginTop: 5 }} />
-                    </Pressable>
-                    <CountryPicker countryCode={countryCode} withFilter withFlag withCountryNameButton withCallingCode visible={show} onClose={() => { setShow(false); }} onSelect={(c) => { setCountryCode(c.cca2); setCountry(c); setShow(false); setUserData((previous) => ({ ...previous, countryCode: "+" + String(c.callingCode), })); }} />
-                
-                </View>
-                
-                <View className="mt-2 bg-red-100 flex flex-row justify-center">
-                    <TextInput inputMode="tel" className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[18%]" placeholder="+94" value={country ? `+${country.callingCode}` : userData.countryCode} onChangeText={ (text)=> { setUserData((previous) => ({ ...previous, countryCode: text, })); }} />
-                    <TextInput inputMode="tel" className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[80%] ml-2" placeholder="77 #### ###" onChangeText={ (text) => { setUserData((previous) => ({ ...previous, contactNo: text, })); }} />
-                
-                </View>
-                <View className="mt-16 w-full">
+                <View className="p-5 items-center">
+                    <View>
+                        <Image source={require("../../assets/logo.png")} className="h-40 w-36" />
+                    </View>
+                    <View>
+                        <Text className="text-slate-600 font-bold">
+                            We use your contacts to help you find friends who are already on the app. Your contacts stay private.
+                        </Text>
+                    </View>
 
-                    <Pressable className="justify-center items-center bg-green-600 w-full h-14 rounded-full" onPress={() => {
-                        navigation.navigate("AvatarScreen");
-                    }}>
-                        <Text className="text-xl font-bold text-slate-50">Next</Text>
-                    
-                    </Pressable>
-                
+                    <View className="mt-5 w-full">
+                        <View className="border-b-2 border-b-green-600 justify-center items-center flex-row h-14 mb-3">
+                            <CountryPicker
+                                countryCode={countryCode}
+                                withFilter
+                                withFlag
+                                withCountryNameButton
+                                withCallingCode
+                                visible={show}
+                                onClose={() => {
+                                    setShow(false);
+                                }}
+                                onSelect={(c) => {
+                                    setCountryCode(c.cca2);
+                                    setCountry(c);
+                                    setShow(false);
+                                    setUserData((previous) => ({
+                                        ...previous,
+                                        countryCode: "+" + String(c.callingCode),
+                                    }));
+                                }}
+                            />
+                            <AntDesign
+                                name="caret-down"
+                                size={24}
+                                color="black"
+                                style={{ marginTop: 5 }}
+                            />
+
+                        </View>
+                    </View>
+
+                    <View className="mt-2 bg-red-100 flex flex-row justify-center">
+                        <TextInput
+                            inputMode="tel"
+                            className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[18%]"
+                            placeholder="+94"
+                            value={country ? `+${country.callingCode}` : callingCode}
+                            onChangeText={(text) => {
+                                setCallingCode(text);
+                            }}
+                        />
+                        <TextInput
+                            inputMode="tel"
+                            className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[80%] ml-2"
+                            placeholder="77 #### ###"
+                            onChangeText={(text) => {
+                                setPhoneNo(text);
+                            }}
+                        />
+                    </View>
+                    <View className="mt-16 w-full">
+                        <Pressable className="justify-center items-center bg-green-600 w-full h-14 rounded-full"
+                            onPress={() => {
+                                const validCountryCode = validateCountryCode(callingCode);
+                                const validPhoneNo = validatePhoneNo(phoneNo);
+
+                                if (validCountryCode) {
+                                    Toast.show({
+                                        type: ALERT_TYPE.WARNING,
+                                        title: "Warning",
+                                        textBody: validCountryCode,
+                                    });
+                                } else if (validPhoneNo) {
+                                    Toast.show({
+                                        type: ALERT_TYPE.WARNING,
+                                        title: "Warning",
+                                        textBody: validPhoneNo,
+                                    });
+                                } else {
+                                    setUserData((previous) => ({ ...previous, countryCode: country ? `+${country.callingCode}` : callingCode, contactNo: phoneNo, }));
+                                    navigation.navigate("AvatarScreen");
+                                }
+                            }}>
+                            <Text className="text-xl font-bold text-slate-50">Next</Text>
+                        </Pressable>
+                    </View>
                 </View>
-            </View>
-        </KeyboardAvoidingView>
-    
-    
-    </SafeAreaView>);
+            </KeyboardAvoidingView>
+
+        </SafeAreaView>
+    );
 }
